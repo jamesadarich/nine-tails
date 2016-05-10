97b986fdf79b932da29439a08a2d49376c604ce1
jamesrichford <jamesrichford@googlemail.com>
2016-05-10 13:12:51 +0100

    Move dev dependencies to correct spot

--- test/rule.spec.ts
+++ test/rule.spec.ts
@@ -1,3 +1,32 @@
+import * as Tape from "tape";
+import * as Sinon from "sinon";
+import { Rule } from "../src/rule";
+import { Style } from "../src/style";
+
+Tape("Rule", (test: Tape.Test) => {
+
+  test.test("linking a style to the Rule", (test: Tape.Test) => {
+     test.test("should handle the set event of the style", (test: Tape.Test) => {
+
+        let styleElement = document.createElement("style");
+        document.head.appendChild(styleElement);
+
+        let styleSheet = <CSSStyleSheet>styleElement.sheet;
+        styleSheet.addRule("zoolander");
+
+        let rule = new Rule(<CSSStyleRule>styleSheet.cssRules[0]);
+
+        let style = new Style();
+        style.onSet = Sinon.spy();
+
+        rule.linkStyle("derelict", style);
+
+        test.equal(true, style.onSet.called);
+
+     });
+  });
+});
+
 /*///<reference path="../typings/jasmine/jasmine.d.ts"/>
 import { Rule } from "../src/rule";
 import { Style } from "../src/style";
@@ -6,15 +35,15 @@ describe("Rule", function() {
    describe("linking a style to the Rule", function () {
       it("should handle the set event of the style", function () {
 
-         var styleElement = document.createElement("style");
+         let styleElement = document.createElement("style");
          document.head.appendChild(styleElement);
 
-         var styleSheet = <CSSStyleSheet>styleElement.sheet;
+         let styleSheet = <CSSStyleSheet>styleElement.sheet;
          styleSheet.addRule("zoolander");
 
-         var rule = new Rule(<CSSStyleRule>styleSheet.cssRules[0]);
+         let rule = new Rule(<CSSStyleRule>styleSheet.cssRules[0]);
 
-         var style = new Style();
+         let style = new Style();
          spyOn(style, "onSet");
 
          rule.linkStyle("derelict", style);
@@ -24,17 +53,17 @@ describe("Rule", function() {
 
       it("should set the css rule to the style value", function () {
 
-         var styleElement = document.createElement("style");
+         let styleElement = document.createElement("style");
          document.head.appendChild(styleElement);
 
-         var styleSheet = <CSSStyleSheet>styleElement.sheet;
+         let styleSheet = <CSSStyleSheet>styleElement.sheet;
          styleSheet.addRule("zoolander");
 
-         var zoolander = <CSSStyleRule>styleSheet.cssRules[0];
+         let zoolander = <CSSStyleRule>styleSheet.cssRules[0];
 
-         var rule = new Rule(zoolander);
+         let rule = new Rule(zoolander);
 
-         var style = new Style();
+         let style = new Style();
          spyOn(style, "get").and.returnValue("blue steel");
 
          rule.linkStyle("derelict", style);
@@ -44,22 +73,22 @@ describe("Rule", function() {
 
       it("should update the style when it is set", function () {
 
-         var styleElement = document.createElement("style");
+         let styleElement = document.createElement("style");
          document.head.appendChild(styleElement);
 
-         var styleSheet = <CSSStyleSheet>styleElement.sheet;
+         let styleSheet = <CSSStyleSheet>styleElement.sheet;
          styleSheet.addRule("zoolander");
 
-         var zoolander = <CSSStyleRule>styleSheet.cssRules[0];
+         let zoolander = <CSSStyleRule>styleSheet.cssRules[0];
 
-         var rule = new Rule(zoolander);
+         let rule = new Rule(zoolander);
 
-         var style = new Style();
+         let style = new Style();
          spyOn(style, "onSet");
 
          rule.linkStyle("derelict", style);
 
-         var update = {
+         let update = {
             newValue: "the tigre"
          };
 
